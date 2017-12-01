60fecbe6d4321f71b85ebd7e4039512897be67ea
AyKarsi <karsten@grombach.com>
2017-12-01 13:24:42 +0100

    localEncryption can handle both byte and string keys

--- src/lib/local/localMasterKey.js
+++ src/lib/local/localMasterKey.js
@@ -1,3 +1,4 @@
+const aesjs = require('aes-js');
 module.exports = function() {
-	return  [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
+	return aesjs.utils.hex.fromBytes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
 };
