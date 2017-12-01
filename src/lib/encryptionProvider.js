29c31e473eccad91ca0076317cc60de9d5e567e8
AyKarsi <karsten@grombach.com>
2017-12-01 15:42:38 +0100

    Updated Dokumentation

--- src/lib/encryptionProvider.js
+++ src/lib/encryptionProvider.js
@@ -3,20 +3,6 @@ const AwsDecrypt = require('./aws/awsDecrypt');
 const LocalEncrypt = require('./local/localEncrypt');
 const LocalDecrypt = require('./local/localDecrypt');
 
-/**
- * @alias module:api
- * @description creates a new cryptari instance
- *
- * @param {Object} [options]
- * @param {String} [options.aws]
- * @param {String} [options.aws.accessKeyId] The IAM user Access ID (who has access to the below key). If present CRYPTARI_AWS_ACCESS_KEY_ID Env Variable will be given precendce.
- * @param {String} [options.aws.secretAccessKey] The matching secret for the IAM user. If present CRYPTARI_AWS_SECRET_ACCESS_KEY Env Variable will be given precendce.
- * @param {String} [options.aws.cmkKeyId] The AWS KMS Key Id. If present CRYPTARI_AWS_CMK_ID Env Variable will be given precendce.
- * @param {String} [options.aws.region] The region where the AWS KMS key is stored. If present CRYPTARI_AWS_REGION Env Variable will be given precendce.
- * @param {String} [options.local]
- * @param {String} [options.local.masterKey] a 16 Byte HexString  used for encrypting locally. If present CRYPTARI_LOCAL_MASTERKEY env variable will be given precedence.
- * @returns {String}
- */
 module.exports = function(config){
 	let provider;
 	//let env = process.env.NODE_ENV;
@@ -29,7 +15,6 @@ module.exports = function(config){
 			provider.decryptDataKey = awsDecrypt.decryptDataKey;
 			provider.decrypt = awsDecrypt.decrypt;
 	}else {
-		console.warn('using local encryption. not intended for production use');
 		provider = LocalEncrypt(config);
 		provider.name = 'local';
 		let localDecrypt = LocalDecrypt(config);
--- src/lib/local/localMasterKey.js
+++ src/lib/local/localMasterKey.js
@@ -5,6 +5,7 @@ module.exports = function(opts) {
 		return CRYPTARI_LOCAL_MASTERKEY;
 	}
 	if (!opts || !opts.local || !opts.local.masterKey){
+		console.warn('using local encryption. not intended for production use');
 		return aesjs.utils.hex.fromBytes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
 	}
 	return opts.local.masterKey;
